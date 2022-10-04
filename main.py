b22cf727b2884bea42a302dbd226b31a734f1008
Guilherme <guilhermemelo@protonmail.com>
2022-10-03 21:43:58 -0300

    Comit #2
    Redefinindo algumas coisas...

--- main.py
+++ main.py
@@ -1,40 +1,47 @@
+'''
+
+About Dataset
+
+Context
+With the advent of Machine Learning algorithms, it is often to find their usage in a wide range of applications.
+Nevertheless, such algorithms require a massive amount of data and several tasks still lake some properly described and
+structured data. In such a context, this dataset aims at providing a unique source of cannabis-related information,
+which have been directly scrapped from Leafly's website.
+
+Content
+The dataset is composed of the encoded data in .json and .csv formats, directly scrapped from Leafly's website. Additionally, it holds all meta-information that was possible to be extracted, as follows:
+
+name;
+image url;
+type;
+thc level;
+most common terpene;
+description;
+positive effects;
+negative effects; and
+diseases it helps with.
+Acknowledgements
+If you use this dataset, we would be greatly thankful if you cite the following repository:
+
+https://github.com/gugarosa/leaflyer
+
+Inspiration
+Fulfill the literature gap in cannabis-related data;
+How to improve cannabis strain classification;
+Do traditional classifiers are often sufficient to overcome such a task;
+How the proposed task might benefit from more modern Computer Vision approaches.
+
+'''
+
 import pandas as pd
 
-# 1 - Carrega uma base de dados pré selecionada
-planetas = pd.read_csv("planets.csv")
+# Lê o arquivo com as genéticas
+tabela_geneticas = pd.read_csv("leafly_strain_data.csv")
 
-# 2 - Operações com a base
+# Separa as geméticas da acordo com a espécie
+sativas = tabela_geneticas.loc[tabela_geneticas["type"] == "Sativa"]
+indicas = tabela_geneticas.loc[tabela_geneticas["type"] == "Indica"]
+hibridas = tabela_geneticas.loc[tabela_geneticas["type"] == "Hybrid"]
 
-# a. Pelo menos um filtro de coluna;
-
-# Pega os dados da terra
-terra = planetas[planetas["Planet"] == "Earth"]
-
-# Exibe os dados da terra
-#print(terra)
-
-
-# b. Pelo menos duas operações de filtro;
-
-# Exibe os planetas cuja distância do sol seja maior do que 1 bilhão de km
-#planetas_distantes = planetas[planetas["Distance from Sun (10^6 km)"] > 1000]
-
-#print(planetas_distantes)
-
-
-# Exibe os planetas maiores em tamanho do que a terra, porém menores em massa
-
-# Pega os planetas maiores que a terra
-planetas_maiores = planetas[planetas["Diameter (km)"] > 12756]
-#print(planetas_maiores)
-
-
-# Pega os planetas menores em masas
-planetas_maiores_e_mais_leves = planetas_maiores[planetas_maiores["Density (kg/m^3)"] < 5514]
-print(planetas_maiores_e_mais_leves)
-
-# c. Pelo menos duas operações de groupby.
-
-
-
-# d. Salvar os dados filtrados em um .csv novo.
+# Separa as melhores genéticas indicas para insônia
+print(sativas.describe())
