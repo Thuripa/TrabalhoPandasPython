6da5e5648637b196b7034d0b25e58dd3a4418ddd
Guilherme <guilhermemelo@protonmail.com>
2022-10-05 06:38:37 -0300

    Comit #4
    socorro

--- main.py
+++ main.py
@@ -1,84 +1,56 @@
 '''
 
-About Dataset
-
-Context
-With the advent of Machine Learning algorithms, it is often to find their usage in a wide range of applications.
-Nevertheless, such algorithms require a massive amount of data and several tasks still lake some properly described and
-structured data. In such a context, this dataset aims at providing a unique source of cannabis-related information,
-which have been directly scrapped from Leafly's website.
-
-Content
-The dataset is composed of the encoded data in .json and .csv formats, directly scrapped from Leafly's website. Additionally, it holds all meta-information that was possible to be extracted, as follows:
-
-name;
-image url;
-type;
-thc level;
-most common terpene;
-description;
-positive effects;
-negative effects; and
-diseases it helps with.
-Acknowledgements
-If you use this dataset, we would be greatly thankful if you cite the following repository:
-
-https://github.com/gugarosa/leaflyer
-
-Inspiration
-Fulfill the literature gap in cannabis-related data;
-How to improve cannabis strain classification;
-Do traditional classifiers are often sufficient to overcome such a task;
-How the proposed task might benefit from more modern Computer Vision approaches.
+1. Entre no Kaggle (https://www.kaggle.com/datasets) e encontre uma base de dados que
+interesse:
+a. Precisa ser em .csv;
+b. Não pode ser muito grande (< 10Mb).
+2. Carregue o .csv da base utilizando a biblioteca Pandas e aplique algumas operações
+com a base:
+a. Pelo menos um filtro de coluna;
+b. Pelo menos duas operações de filtro;
+c. Pelo menos duas operações de groupby.
+d. Salvar os dados filtrados em um .csv novo.
+3. Imprima gráficos dessa base (filtrada ou do groupby) utilizando a matplotlib.
+a. Pelo menos 3 gráficos.
 
 '''
 
 import pandas as pd
 
-
-
-import matplotlib as plt
-
 # Lê o arquivo com as genéticas
 tabela_geneticas = pd.read_csv("leafly_strain_data.csv")
 
-pd.set_option('max_colwidth', 10)
-pd.set_option('max_colwidth', 10)
-pd.set_option('max_colwidth', 10)
+#pd.set_option('max_colwidth', 10)
 
+# a. Pelo menos um filtro de coluna;
 # Separa as genéticas da acordo com a coluna espécie (type)
 sativas = tabela_geneticas.loc[tabela_geneticas["type"] == "Sativa"]
 indicas = tabela_geneticas.loc[tabela_geneticas["type"] == "Indica"]
 hibridas = tabela_geneticas.loc[tabela_geneticas["type"] == "Hybrid"]
 
-# Separa as genéticas com eficácia no tratamento da depressão
-geneticas_depressao = tabela_geneticas.loc[tabela_geneticas['depression'] != "0%"]
+# b. Pelo menos duas operações de filtro;
+# Separa as genéticas indicas com eficácia no tratamento da depressão
+#indicas_depressao = indicas.loc[indicas['depression'] != "0%",
+                                         #['name', 'type', 'depression', 'thc_level']]
+#print(indicas_depressao)
 
-geneticas_depressao.to_csv("geneticas_depressao.csv")
+# Separa as genéticas com eficácia no tratamento do câncer
+#geneticas_cancer = tabela_geneticas.loc[tabela_geneticas['cancer'] != "0%", ['name', 'type', 'cancer', 'thc_level']]
+#print(geneticas_cancer)
+
+
+# Registra a tabela em um arquivo csv
+#indicas_depressao.to_csv("geneticas_depressao.csv")
+
+
+# group by
+grupopor = tabela_geneticas.groupby('name').sum()
+print(grupopor)
 
 # Separa as genéticas indicas com efeito calmante e que não causam sono
 
 
 
-# Extrai as colunas de nome e nota para tratamento de depressão
-lista_nomes = geneticas_depressao['name'].to_list()
-lista_notas = geneticas_depressao['depression'].to_list()
 
-# Cria um dicionário contendo o nome e nota
-dicionario = {'nome', 'nota'}
-
-# Popula o dicionario com o nome e nota
-for x in range(0 , len(lista_notas)):
-
-    # Recebe a nota da genética x
-    nota = lista_notas[x]
-
-    # Cria uma tupla com o nome e nota da genética x
-    genetica = (lista_nomes[x], nota[0: -1])
-
-    # Adiciona no dicionario
-    dicionario.add(genetica)
-
-print(dicionario)
 
 
